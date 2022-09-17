f9e617bac16a7775b3bc911cfbfdfd4d6f19d544
Danil <danilsmy.edu@gmail.com>
2022-09-17 18:36:53 +0300

    Add mazeReader

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace behavioralPattern
+namespace Lab1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string path = @"D:\Education\PIIS\PIISLabs\Lab1\Lab1\Maze.txt";
+            mazeReader check = new mazeReader(path);
         }
     }
 }
---
+++ Lab1/Lab1/mazeReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class mazeReader
+    {
+        public List<List<int>> Maze = new List<List<int>>();
+        public mazeReader(string path)
+        {
+            string[] lst = File.ReadAllText(path).Split(' ');
+            int counter = 2;
+            int rows = Convert.ToInt32(lst[0]);
+            int columns = Convert.ToInt32(lst[1]);
+            for (int i = 0; i < rows; i++)
+            {
+                List<int> temp = new List<int>();
+                for (int j = 0; j < columns; j++)
+                {
+                    
+                    temp.Add(Convert.ToInt32(lst[counter++]));
+                    
+                }
+                Maze.Add(temp);
+            }
+            Console.WriteLine("The size of Maze: {0}x{1} \n", rows, columns);
+            Console.WriteLine("Our Maze:");
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    Console.Write(" {0}",Maze[i][j]);
+                }
+                Console.WriteLine() ;
+            }
+        }
+    }
+}
