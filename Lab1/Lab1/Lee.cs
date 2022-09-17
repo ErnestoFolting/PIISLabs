546b92901146f9643e2d692f7838243f5d7ff1cb
Danil <danilsmy.edu@gmail.com>
2022-09-17 20:10:01 +0300

    Add some Lee algo code

---
+++ Lab1/Lab1/Lee.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+struct cell
+{
+    public int i;
+    public int j;
+}
+namespace Lab1
+{
+
+    internal class Lee
+    {
+        public List<List<int>> Maze = new List<List<int>>(); 
+        public List<List<bool>> visited = new List<List<bool>>();
+        public Lee(List<List<int>> maze)
+        {
+            Maze = maze;
+            for(int i = 0;i< maze.Count; i++)
+            {
+                List<bool> temp = new List<bool>();
+                for (int j = 0; j < maze[i].Count; j++)
+                {
+                    temp.Add(false);
+                }
+                visited.Add(temp);
+            }
+        }
+        public bool isCorrect(cell cellToCheck)
+        {
+            return (cellToCheck.i >= 0 && cellToCheck.i < Maze[0].Count && cellToCheck.j >= 0 && cellToCheck.j < Maze.Count);
+        }
+        public cell findFinalCell(cell start, cell final)
+        {
+
+            if (!(isCorrect(start) && isCorrect(final) && Maze[start.i][start.j] == 1 && Maze[final.i][final.j] == 1))
+            {
+                throw new Exception("Incorrect coodinates!");
+            }
+            visited[start.i][start.j] = true;
+            Queue<cell> q = new Queue<cell>();
+            q.Enqueue(start);
+            int waveNumber = 0;
+            while (q.Count != 0)
+            {
+                waveNumber++;
+                cell cur = q.Dequeue();
+                if(cur.i == final.i && cur.j == final.j)
+                {
+                    Console.WriteLine("We found the final cell! Coords is: [{0},{1}]. Distance is: {2}",cur.i,cur.j,waveNumber);
+                    return cur;
+                }
+
+                cell temp = new cell();
+                temp.i = cur.i + 1;
+                temp.j = cur.j;
+                if (isCorrect(temp) && Maze[temp.i][temp.j] == 1 && visited[temp.i][temp.j] == false)
+                {
+                    visited[temp.i][temp.j] = true;
+                    q.Enqueue(temp);
+                }
+                temp.i = cur.i - 1;
+                temp.j = cur.j;
+                if (isCorrect(temp) && Maze[temp.i][temp.j] == 1 && visited[temp.i][temp.j] == false)
+                {
+                    visited[temp.i][temp.j] = true;
+                    q.Enqueue(temp);
+                }
+                temp.i = cur.i;
+                temp.j = cur.j + 1;
+                if (isCorrect(temp) && Maze[temp.i][temp.j] == 1 && visited[temp.i][temp.j] == false)
+                {
+                    visited[temp.i][temp.j] = true;
+                    q.Enqueue(temp);
+                }
+                temp.i = cur.i;
+                temp.j = cur.j - 1;
+                if (isCorrect(temp) && Maze[temp.i][temp.j] == 1 && visited[temp.i][temp.j] == false)
+                {
+                    visited[temp.i][temp.j] = true;
+                    q.Enqueue(temp);
+                }
+            }
+            Console.WriteLine("NOT FOUND");
+            return default;
+        }
+    }
+}
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -8,7 +8,20 @@ namespace Lab1
         static void Main(string[] args)
         {
             string path = @"D:\Education\PIIS\PIISLabs\Lab1\Lab1\Maze.txt";
-            mazeReader check = new mazeReader(path);
+            mazeReader reader = new mazeReader(path);
+            List<List<int>> Maze = reader.Maze;
+            Lee alg = new Lee(Maze);
+            cell start = new cell();
+            cell final = new cell();
+            Console.WriteLine("\nInput start cell i:");
+            start.i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input start cell j:");
+            start.j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input final cell i:");
+            final.i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input final cell j:");
+            final.j = Convert.ToInt32(Console.ReadLine());
+            alg.findFinalCell(start,final);
         }
     }
 }
