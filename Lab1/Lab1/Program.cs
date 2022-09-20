b31dd76932e6b639bee381212f3d59906250d647
Danil <danilsmy.edu@gmail.com>
2022-09-20 11:56:17 +0300

    Correct menu integration

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -10,29 +10,42 @@ namespace Lab1
             string path = @"D:\Education\PIIS\PIISLabs\Lab1\Lab1\Maze.txt";
             mazeReader reader = new mazeReader(path);
             List<List<int>> Maze = reader.Maze;
-            Lee alg = new Lee(Maze);
-            cell start = new cell();
-            cell final = new cell();
+
             Console.WriteLine("\nInput start cell i:");
-            start.i = Convert.ToInt32(Console.ReadLine());
+            int startI = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Input start cell j:");
-            start.j = Convert.ToInt32(Console.ReadLine());
+            int startJ = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Input final cell i:");
-            final.i = Convert.ToInt32(Console.ReadLine());
+            int finalI = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Input final cell j:");
-            final.j = Convert.ToInt32(Console.ReadLine());
-            cell foundFinal = alg.findFinalCell( start,final);
-            alg.buildPath(start,foundFinal);
-            /*****************A-STAR ALGO******************/
-            Point start2 = new Point();
-            Point final2 = new Point();
-            start2.i = start.i;
-            start2.j = start.j;
-            final2.i = final.i;
-            final2.j = final.j;
-            AStar astar = new AStar(Maze,final2);
-            Point foundFinal2 = astar.findFinal(start2);
-            astar.buildPath(start2,foundFinal2);
+            int finalJ = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose the algo: Lee(0) A-Star(1)");
+            int choose = Convert.ToInt32((Console.ReadLine()));
+            if(choose == 0)
+            {
+                Lee alg = new Lee(Maze);
+                cell start = new cell();
+                cell final = new cell();
+                start.i = startI;
+                start.j = startJ;
+                final.i = finalI;
+                final.j = finalJ;
+                cell foundFinal = alg.findFinalCell(start, final);
+                alg.buildPath(start, foundFinal);
+            }
+            else
+            {
+                Point start2 = new Point();
+                Point final2 = new Point();
+                start2.i = startI;
+                start2.j = startJ;
+                final2.i = finalI;
+                final2.j = finalJ;
+                AStar astar = new AStar(Maze, final2);
+                Point foundFinal2 = astar.findFinal(start2);
+                astar.buildPath(start2, foundFinal2);
+            } 
         }
     }
 }
